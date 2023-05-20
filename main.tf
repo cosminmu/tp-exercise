cb79b1f10cc0ed813f6db4a396c49d66fa8b6f03
Cosmin Muscalu <cos.az@outlook.com>
2023-05-21 00:10:37 +0100

    adding working-solution simple branch

--- main.tf
+++ main.tf
@@ -1,39 +1,34 @@
 resource "azurerm_resource_group" "tp-exercise" {
-  for_each = var.environments
-  name     = "${each.key}-terraform-servicebus"
+  name     = "${terraform.workspace}-tpex-terraform-servicebus"
   location = "West Europe"
 }
 
-/*
 resource "azurerm_servicebus_namespace" "tp-exercise" {
-  for_each = var.environments
-  name                = "${each.key}--${each.value.asbn_names}-servicebus-namespace"
-  location            = azurerm_resource_group.tp-exercise[0].location
-  resource_group_name = azurerm_resource_group.tp-exercise[0].name
+  name                = "${terraform.workspace}-tpex-servicebus-namespace"
+  location            = azurerm_resource_group.tp-exercise.location
+  resource_group_name = azurerm_resource_group.tp-exercise.name
   sku                 = "Standard"
 
   tags = {
-    costcentre = "terraform"
-    procuctname = "tp-exercise"
+    costcentre = "${terraform.workspace}-terraform"
+    productname = "tp-exercise"
   }
 }
-*/
 
-/*
 resource "azurerm_servicebus_queue" "tp-exercise" {
-  name         = "tfex_servicebus_queue"
+  name         = "${terraform.workspace}-tpex_servicebus_queue"
   namespace_id = azurerm_servicebus_namespace.tp-exercise.id
 
   enable_partitioning = true
 }
 
-resource "azurerm_servicebus_queue" "tp-exercise2" {
-  name         = "tfex_servicebus_queue2"
+resource "azurerm_servicebus_queue" "tp-exercise1" {
+  name         = "${terraform.workspace}-tpex_servicebus_queue2"
   namespace_id = azurerm_servicebus_namespace.tp-exercise.id
 
   enable_partitioning = true
 }
-
+ 
 resource "azurerm_management_lock" "resource-group-level" {
   name       = "resource-group-level"
   scope      = azurerm_resource_group.tp-exercise.id
@@ -41,4 +36,40 @@ resource "azurerm_management_lock" "resource-group-level" {
   notes      = "This Resource Group is Read-Only"
 }
 
-*/
+resource "azurerm_resource_group" "tp-exercise2" {
+  name     = "${terraform.workspace}-tpex2-terraform-servicebus"
+  location = "West Europe"
+}
+
+resource "azurerm_servicebus_namespace" "tp-exercise2" {
+  name                = "${terraform.workspace}-tpex2-servicebus-namespace"
+  location            = azurerm_resource_group.tp-exercise2.location
+  resource_group_name = azurerm_resource_group.tp-exercise2.name
+  sku                 = "Standard"
+
+  tags = {
+    costcentre = "${terraform.workspace}-terraform"
+    productname = "tp-exercise"
+  }
+}
+
+resource "azurerm_servicebus_queue" "tp-exercise2" {
+  name         = "${terraform.workspace}-tpex2_servicebus_queue"
+  namespace_id = azurerm_servicebus_namespace.tp-exercise2.id
+
+  enable_partitioning = true
+}
+
+resource "azurerm_servicebus_queue" "tp-exercise2-1" {
+  name         = "${terraform.workspace}-tpex2_servicebus_queue2"
+  namespace_id = azurerm_servicebus_namespace.tp-exercise2.id
+
+  enable_partitioning = true
+}
+
+resource "azurerm_management_lock" "resource-group-level2" {
+  name       = "resource-group-level"
+  scope      = azurerm_resource_group.tp-exercise2.id
+  lock_level = "ReadOnly"
+  notes      = "This Resource Group is Read-Only"
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+To whom it may concern
+
+I  understand the brief of the exercise however I have come to realise that building that nested data structure would necessitate more than an hour's work and I have made a head start of the main branch which is incomplete, however I have also built a solution that works and delivers the required resources using Terraform but with a model that is simpler, this is what I can deliver quickly. To be honest I probably could use a bit of pair programming for the required nested data structure.
+
+Please checkout the branch: working-solution  -> for a full working solution - the working hard version :)
+
+In working-solution there are 3 terraform workspaces, once for each environment: test, staging, production.
